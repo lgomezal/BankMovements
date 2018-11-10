ceca6780e64cc6a8b93bc94f581ba97b22c5eab5
LuisGomezAlonso <lgomezal@gmail.com>
2018-11-10 11:05:12 +0100

    Initial Commit

---
+++ BankMovements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BankMovements
+//
+//  Created by luis gomez alonso on 10/11/2018.
+//  Copyright © 2018 luis gomez alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
